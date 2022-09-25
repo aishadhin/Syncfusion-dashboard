0a72be7a6b50ce33592f1fb5ecc92e32419943e5
Ai Shadhin <aishadhin.ac@gmail.com>
2022-09-25 14:50:27 -0500

    route and components created

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
 
 const App = () => {
-
+    const activeMenu = false;
     return (
         <div>
             <BrowserRouter>
@@ -13,14 +13,61 @@ const App = () => {
                     <div className='fixed right-3 bottom-4' style={{ zIndex: '1000' }}>
                         <TooltipComponent content='Settings' position='Top'>
                             <button type='button'
-                            className='text-3xl p-3 hover'>
-                                <FiSettings/>
+                                className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background: 'blue', borderRadius: '50%' }}>
+                                <FiSettings />
                             </button>
                         </TooltipComponent>
                     </div>
+                    {activeMenu
+                        ?
+                        (
+                            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                                sidebar
+                            </div>
+                        )
+                        :
+                        (
+                            <div className='w-0 dark:bg-secondary-dark-bg'>
+                                sidebar
+                            </div>
+                        )
+                    }
+                    <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`} >
+                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                            navbar
+                        </div>
+                    </div>
+                    <div>
+                        <Routes>
+                            {/* Dashboard */}
+                            <Route path='/' element='Ecommerce'/>
+                            <Route path='/ecommerce' element='Ecommerce'/>
+
+                            {/* Pages */}
+                            <Route path='/orders' element='Orders'/>
+                            <Route path='/employee' element='Employee'/>
+                            <Route path='/customers' element='Customers'/>
+
+                            {/* Apps */}
+                            <Route path='/kanban' element='Kanban'/>
+                            <Route path='/editor' element='Editor'/>
+                            <Route path='/color-picker' element='ColorPicker'/>
+
+                            {/* Charts */}
+                            <Route path='/line' element='Line'/>
+                            <Route path='/area' element='Area'/>
+                            <Route path='/bar' element='Bar'/>
+                            <Route path='/pie' element='Pie'/>
+                            <Route path='/financial' element='Financial'/>
+                            <Route path='/color-mapping' element='ColorMapping'/>
+                            <Route path='/pyramid' element='Pyramid'/>
+                            <Route path='/stacked' element='Stacked'/>
+                        </Routes>
+                    </div>
                 </div>
-            </BrowserRouter>
-        </div>
+            </BrowserRouter >
+        </div >
+
     );
 };
 
