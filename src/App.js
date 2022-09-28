6e66370a9f87245d830b824e1366792a8f26d6ee
Ai Shadhin <aishadhin.ac@gmail.com>
2022-09-28 10:34:27 -0500

    earning and state section done

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { useStateContext } from './context/contextProvider';
 
 const App = () => {
 
-    const {activeMenu} = useStateContext();
+    const { activeMenu } = useStateContext();
 
 
     return (
@@ -29,47 +29,48 @@ const App = () => {
                         ?
                         (
                             <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-                                <Sidebar/>
+                                <Sidebar />
                             </div>
                         )
                         :
                         (
                             <div className='w-0 dark:bg-secondary-dark-bg'>
-                                <Sidebar/>
+                                <Sidebar />
                             </div>
                         )
                     }
                     <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`} >
                         <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                            <Navbar/>
+                            <Navbar />
                         </div>
-                    </div>
-                    <div>
-                        <Routes>
-                            {/* Dashboard */}
-                            <Route path='/' element={<Ecommerce/>}/>
-                            <Route path='/ecommerce' element={<Ecommerce/>}/>
 
-                            {/* Pages */}
-                            <Route path='/orders' element={<Orders/>}/>
-                            <Route path='/employees' element={<Employees/>}/>
-                            <Route path='/customers' element={<Customers/>}/>
+                        <div>
+                            <Routes>
+                                {/* Dashboard */}
+                                <Route path='/' element={<Ecommerce />} />
+                                <Route path='/ecommerce' element={<Ecommerce />} />
 
-                            {/* Apps */}
-                            <Route path='/kanban' element={<Kanban/>}/>
-                            <Route path='/editor' element={<Editor/>}/>
-                            <Route path='/color-picker' element={<ColorPicker/>}/>
+                                {/* Pages */}
+                                <Route path='/orders' element={<Orders />} />
+                                <Route path='/employees' element={<Employees />} />
+                                <Route path='/customers' element={<Customers />} />
 
-                            {/* Charts */}
-                            <Route path='/line' element={<Line/>}/>
-                            <Route path='/area' element={<Area/>}/>
-                            <Route path='/bar' element={<Bar/>}/>
-                            <Route path='/pie' element={<Pie/>}/>
-                            <Route path='/financial' element={<Financial/>}/>
-                            <Route path='/color-mapping' element={<ColorMapping/>}/>
-                            <Route path='/pyramid' element={<Pyramid/>}/>
-                            <Route path='/stacked' element={<Stacked/>}/>
-                        </Routes>
+                                {/* Apps */}
+                                <Route path='/kanban' element={<Kanban />} />
+                                <Route path='/editor' element={<Editor />} />
+                                <Route path='/color-picker' element={<ColorPicker />} />
+
+                                {/* Charts */}
+                                <Route path='/line' element={<Line />} />
+                                <Route path='/area' element={<Area />} />
+                                <Route path='/bar' element={<Bar />} />
+                                <Route path='/pie' element={<Pie />} />
+                                <Route path='/financial' element={<Financial />} />
+                                <Route path='/color-mapping' element={<ColorMapping />} />
+                                <Route path='/pyramid' element={<Pyramid />} />
+                                <Route path='/stacked' element={<Stacked />} />
+                            </Routes>
+                        </div>
                     </div>
                 </div>
             </BrowserRouter >
