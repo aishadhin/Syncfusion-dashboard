ce3abd1a27a2211e6133554973b3e25c54b759e4
Ai Shadhin <aishadhin.ac@gmail.com>
2022-09-28 14:14:12 -0500

    sparkLine added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Button = ({bgColor, color, size, text, borderRadius}) => {
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
     return (
-        <button type='button' style={{backgroundColor:bgColor, color, borderRadius}}
-        className={`text-${size} p-3 hover:drop-shadow-xl`}
+        <button type='button' style={{ backgroundColor: bgColor, color, borderRadius }}
+            className={`text-${size} p-3 hover:drop-shadow-xl`}
         >
-{text}
+            {text}
         </button>
     );
 };
